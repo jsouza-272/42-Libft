9e8d707e06e2b971773bebb9a2a1317ab742ccbc
Joao Souza <jsouza@c1r6s9.42lisboa.com>
2025-11-07 16:29:54 +0000

    update

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jsouza <jsouza@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 16:16:57 by jsouza            #+#    #+#             */
-/*   Updated: 2025/11/06 15:10:57 by jsouza           ###   ########.fr       */
+/*   Updated: 2025/11/07 10:10:15 by jsouza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst)
+	if (!lst || !new)
 		return ;
 	new->next = *lst;
 	*lst = new;
